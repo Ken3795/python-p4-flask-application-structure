eab9300a39e7c801f01c2b0a007478d07b19f6da
Kenneth Kipkoech <kenkipkoech57@gmail.com>
2025-03-26 12:10:13 +0300

    lab completed

--- server/app.py
+++ server/app.py
@@ -1,5 +1,18 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+# Route for the homepage
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+# Route for dynamic user profile
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+# Run the application if this script is executed directly
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
+
